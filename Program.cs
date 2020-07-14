8b27a695cb264ef8dc91b92a1d45aca5c6059f67
patricia <patricia.selpa@gmail.com>
2020-07-13 22:03:12 -0300

    novas descobertas

--- Program.cs
+++ Program.cs
@@ -10,27 +10,43 @@ namespace primeira_aula
             
             //Console.WriteLine(currentDate);
 
-            Console.WriteLine("Qual a sua idade?");
-            var result = Console.ReadLine();
+            // Console.WriteLine("Qual a sua idade?");
+            // var result = Console.ReadLine();
             
-            //Transformando o texto que usuário digitou para um tipo númerico
-            int userAge = Int32.Parse(result);
+            // //Transformando o texto que usuário digitou para um tipo númerico
+            // int userAge = Int32.Parse(result);
 
-            //comparando SE a idade do usuário é maior que 15 E menor que 51            
-            if (userAge > 15 && userAge < 51)
+            // //comparando SE a idade do usuário é maior que 15 E menor que 51            
+            // if (userAge > 15 && userAge < 51)
+            // {
+            //    Console.WriteLine("Sim, você tem idade para o Entra21");
+            // }
+            // else if (userAge == 14 || userAge == 15)
+            // { 
+            // Console.WriteLine("Você não têm idade para o Entra21, mas têm para ser menor aprediz");
+            // } 
+            // else if (userAge > 50)
+            // {    
+            // Console.WriteLine("Não, você não têm idade para o Entra21");
+            // }
+            // Console.WriteLine("Só isso, mais nada");
+
+            var names = new string [3];
+            var counter = 0;
+
+            while (counter < 3)
             {
-               Console.WriteLine("Sim, você tem idade para o Entra21");
+                System.Console.WriteLine("Digite um nome");
+                var result = Console.ReadLine();
+
+                names[counter] = result;
+                counter++;
             }
-            else if (userAge == 14 || userAge == 15)
-            { 
-            Console.WriteLine("Você não têm idade para o Entra21, mas têm para ser menor aprediz");
-            } 
-            else if (userAge > 50)
-            {    
-            Console.WriteLine("Não, você não têm idade para o Entra21");
-            }
-            Console.WriteLine("Só isso, mais nada");
-           
+
+            System.Console.WriteLine($"Olá,{names[0]}!");
+            System.Console.WriteLine($"Olá,{names[1]}!");
+            System.Console.WriteLine($"Olá,{names[2]}!");
+                       
         }
     }
 }
